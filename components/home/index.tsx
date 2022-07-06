d7c53459085f841fa3238afc06ed2f919bccd2b2
Oleksandr <algolj@algolj.it>
2022-07-07 00:26:57 +0300

    feat: implement nav and the main functionality of the header

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -4,11 +4,13 @@ import Image from 'next/image';
 import homeImg from '../../public/assets/img/girl-and-pet.png';
 
 import style from './style.module.scss';
+import useLocale from '../../hooks/useLocale';
 
 const Home: FC = () => {
+  const locale = useLocale();
   return (
     <section className={style.home}>
-      <Image src={homeImg} layout="fill" />
+      <Image src={homeImg} layout="fill" alt={locale.hugCat} />
     </section>
   );
 };
