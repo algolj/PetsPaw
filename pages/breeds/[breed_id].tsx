b065302fae33baedd501a18982717a73b208b5ba
Oleksandr <algolj@algolj.it>
2022-07-12 20:50:12 +0300

    feat: implement SSG for the pages with breed

--- pages/breeds/[breed_id].tsx
+++ pages/breeds/[breed_id].tsx
@@ -1,48 +1,73 @@
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 import { NextPage } from 'next';
 
 import useLocale from '../../hooks/useLocale';
 
-import { useGetBreedQuery } from '../../store';
-
-import Loader from '../../components/loader';
 import BreedInfo from '../../components/breedInfo';
 import MainLayout from '../../components/mainLayout';
 import PagePanel from '../../components/pagePanel';
 import NotFoud from '../../components/notFound';
-
-import style from './style.module.scss';
 import ImageContainer from '../../components/imageContainer';
 
-const Breeds: NextPage = () => {
-  const locale = useLocale();
+import style from './style.module.scss';
 
-  const router = useRouter();
+import { constants } from '../../constants';
 
-  const { data: breed, isLoading } = useGetBreedQuery(
-    router.query as { breed_id: string },
+import { IBreed } from '../../interfaces/breed.interface';
+
+export async function getStaticPaths() {
+  const path = await fetch(`${constants.API_URL}breeds`).then((req) =>
+    req.json(),
   );
+  return {
+    paths: (path as IBreed[]).map(({ id }) => ({
+      params: { breed_id: String(id) },
+    })),
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({
+  params,
+}: {
+  params: { breed_id: string };
+}) {
+  const breed = await fetch(
+    `${constants.API_URL}images/search?breed_id=${params.breed_id}`,
+    {
+      headers: {
+        'x-api-key': constants.API_KEY,
+      },
+    },
+  )
+    .then((req) => req.json())
+    .then((breedArr) => ({
+      url: breedArr?.[0]?.url || '',
+      breedInfo: breedArr[0]?.breeds?.[0] || {},
+    }));
+  return { props: { ...breed } };
+}
+
+const Breeds: NextPage<{ breedInfo: IBreed; url: string }> = ({
+  breedInfo,
+  url,
+}) => {
+  const locale = useLocale();
 
   return (
     <MainLayout>
       <Head>
-        <title>breed · PetsPaw</title>
+        <title>Breed · PetsPaw</title>
       </Head>
 
       <PagePanel title={locale.breeds} isSubTitle={true} href="breeds">
         <p className={style['breed-title']}>
-          {breed?.[0]?.breeds[0]?.id || (!isLoading ? locale.noFound : '')}
+          {breedInfo?.id || locale.noFound}
         </p>
       </PagePanel>
-      {isLoading ? (
-        <Loader />
-      ) : breed?.[0]?.breeds[0] ? (
+      {breedInfo?.id ? (
         <>
-          <ImageContainer
-            image={breed?.[0].url}
-            alt={breed?.[0].breeds[0].name}
-          >
+          <ImageContainer image={url} alt={breedInfo.name}>
             <div className={style.carousel}>
               {Array(5)
                 .fill('')
@@ -51,7 +76,7 @@ const Breeds: NextPage = () => {
                 ))}
             </div>
           </ImageContainer>
-          <BreedInfo {...breed?.[0].breeds[0]} />
+          <BreedInfo {...breedInfo} />
         </>
       ) : (
         <NotFoud />
